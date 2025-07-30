30c462397d99aee8d27d7f1569db50e0be80c421
pravindevp <praveensivakumar55@gmail.com>
2025-07-30 23:44:39 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ommurugantemple/', // <-- Repo name here (top-level)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
